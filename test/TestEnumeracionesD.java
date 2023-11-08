d70faa31bb7278387eaa3d6ce561dcc2131e4dc6
Juan Santiago <142945221+Juanpabloohio@users.noreply.github.com>
2023-11-08 13:29:22 -0500

    Add files via upload

---
+++ test/TestEnumeracionesD.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TestDias;
+
+import enumeracionesd.*;
+
+public class TestEnumeracionesD {
+  
+    public static void main(String[] args) {
+        System.out.println(Dias.Domingo);
+        System.out.println(Dias.Viernes);
+    }
+}
